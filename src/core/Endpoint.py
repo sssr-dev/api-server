688ad77111ae2a355bccddb891482a899c5c1e2e
SantaSpeen <admin@ahegao.ovh>
2022-03-15 14:14:32 +0300

    Config add git_counter and methods

--- src/core/Endpoint.py
+++ src/core/Endpoint.py
@@ -3,15 +3,17 @@ from typing import Union
 
 class Endpoint:
 
+    import_name: str = None
+
     name: str = None
     endpoint: str = None
-    methods: list = ["GET"]
+    methods: list = None
     version: Union[str, int] = None
     need_auth: bool = False
-    db_config: dict = dict()
+    db_config: dict = None
 
     def __repr__(self) -> str:
-        return f"<Endpoint name='{self.name}' endpoint='{self.endpoint}' methods={self.methods} version='{self.version}' need_auth={self.need_auth} db_config={self.db_config}>"
+        return f"<Endpoint '{self.import_name}' name='{self.name}' endpoint='{self.endpoint}' methods={self.methods} version='{self.version}' need_auth={self.need_auth} db_config={self.db_config}>"
 
     def __dict__(self) -> dict:
         return {"rule": self.endpoint, "methods": self.methods}
